3b29ba7bb2b35489b3e20f7bc657e547b29ee728
deborabeatrizf <deborabeatriz.f@gmail.com>
2025-02-22 07:19:53 -0300

    update protected route index file

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,11 +1,23 @@
-import { Route, Redirect } from "react-router-dom";
+// import { Route, Redirect } from "react-router-dom";
 
-function ProtectedRoute({ isLoggedIn, children, ...props }) {
-  return (
-    <Route {...props}>
-      { isLoggedIn ? children : <Redirect to="/login" /> }
-    </Route>
-  )
+// function ProtectedRoute({ isLoggedIn, children, ...props }) {
+//   return (
+//     <Route {...props}>
+//       { isLoggedIn ? children : <Redirect to="/login" /> }
+//     </Route>
+//   )
+// }
+
+// export default ProtectedRoute;
+
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ isLoggedIn, children }) {
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
 }
 
 export default ProtectedRoute;
