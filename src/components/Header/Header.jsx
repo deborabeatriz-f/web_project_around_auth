3c2d18262b80404dccd9e9aa75811a9f09f1d2e0
deborabeatrizf <deborabeatriz.f@gmail.com>
2025-02-22 13:59:31 -0300

    update header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,29 @@
 import logo from "../../images/logo_around.png";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-export default function Header() {
+export default function Header({ text }) {
+  const { currentUser, loggedIn, setLoggedIn } = useContext(CurrentUserContext);
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+    setLoggedIn(false);
+  };
+
   return (
     <header className="header page__container">
       <img src={logo} alt="Logo Around The U.S." className="header__image" />
+      {loggedIn ? (
+        <div>
+          <p>{currentUser.email}</p>
+          <button onClick={handleLogOut}>Sair</button>{" "}
+        </div>
+      ) : (
+        <a href={text === "Entrar" ? "/login" : "/register"}>{text}</a>
+      )}
     </header>
   );
 }
