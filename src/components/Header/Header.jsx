e5460b86e024c5186e0f3948ac0478c61a2ccae6
deborabeatrizf <deborabeatriz.f@gmail.com>
2025-02-25 17:50:33 -0300

    update header jsx file - add logout button and menu button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,61 @@
 import logo from "../../images/logo_around.png";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { removeToken } from "../../utils/token";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 export default function Header({ text }) {
-  const { currentUser, loggedIn, setLoggedIn } = useContext(CurrentUserContext);
+  const { currentUser, isLoggedIn, setIsLoggedIn } =
+    useContext(CurrentUserContext);
   const navigate = useNavigate();
 
   const handleLogOut = () => {
-    localStorage.removeItem("token");
+    removeToken();
     navigate("/login");
-    setLoggedIn(false);
+    setIsLoggedIn(false);
+  };
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleToggleMenu = () => {
+    setIsMenuOpen((preview) => !preview);
   };
 
   return (
-    <header className="header page__container">
-      <img src={logo} alt="Logo Around The U.S." className="header__image" />
-      {loggedIn ? (
-        <div>
-          <p>{currentUser.email}</p>
-          <button onClick={handleLogOut}>Sair</button>{" "}
+    <>
+      {isMenuOpen && (
+        <div className="header__mobile-logout">
+          <p className="header__email">{currentUser.email}</p>
+          <button className="header__button-logout" onClick={handleLogOut}>
+            Sair
+          </button>
         </div>
-      ) : (
-        <a
-          className="header__link"
-          href={text === "Entrar" ? "/login" : "/register"}
-        >
-          {text}
-        </a>
       )}
-    </header>
+      <header className="header page__container">
+        <img src={logo} alt="Logo Around The U.S." className="header__image" />
+        {isLoggedIn ? (
+          <>
+            <button
+              className={
+                isMenuOpen ? "header__close-menu" : "header__open-menu"
+              }
+              onClick={() => handleToggleMenu()}
+            ></button>
+            <div className="header__logout">
+              <p className="header__email">{currentUser.email}</p>
+              <button className="header__button-logout" onClick={handleLogOut}>
+                Sair
+              </button>{" "}
+            </div>
+          </>
+        ) : (
+          <a
+            className="header__link"
+            href={text === "Entrar" ? "/login" : "/register"}
+          >
+            {text}
+          </a>
+        )}
+      </header>
+    </>
   );
 }
